3516727db60e46f5ce1ec539c1862de7b524f388
umermemon4648 <umermemon4648@gmail.com>
2023-03-27 10:49:56 +0500

    creating new files

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import {getRandomPrompt} from '../utils'
+import { preview } from '../assets'
+import { Loader, Card, FormField  } from '../components'
 
 const CreatePost = () => {
   return (
